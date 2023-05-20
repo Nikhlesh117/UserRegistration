94392bf7afa06311cd88126f7acd70e7e481104a
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-05-20 20:55:56 +0530

    Added: User Registration

---
+++ RegexDemoo/RegexDemoo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegexDemoo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Registration registration = new Registration();
+            Console.Write("Enter Your First Name : ");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(registration.FirstName(FirstName));
+
+            Console.WriteLine("Enter Your First Name : ");
+            string LastName = Console.ReadLine();
+            Console.WriteLine(registration.LastName(LastName));
+
+            Console.WriteLine("Enter Your Email ID");
+            string Email = Console.ReadLine();
+            Console.WriteLine(registration.Email(Email));
+
+            Console.WriteLine("Enter Your Phone Number");
+            string PhoneNumber = Console.ReadLine();
+            Console.WriteLine(registration.PhoneNumber(PhoneNumber));
+
+            Console.WriteLine("Enter your Password one");
+            string Password_one = Console.ReadLine();
+            Console.WriteLine(registration.Password_one(Password_one));
+
+            Console.WriteLine("Enter your Password Two");
+            string password_two = Console.ReadLine();
+            Console.WriteLine(registration.Password_Two(password_two));
+
+            Console.WriteLine("Enter Your Password");
+            string password3 = Console.ReadLine();
+            Console.WriteLine(registration.Password_Three(password3));
+
+            Console.WriteLine("Enter Your Password");
+            string password4 = Console.ReadLine();
+            Console.WriteLine(registration.Password_Four(password4));
+
+            registration.Test();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegexDemoo/RegexDemoo/Registration.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemoo
+{
+    public class Registration
+    {
+        public static string Regex_FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string Regex_LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public const string Regex_Email = "^[0-9A-Za-z]+([._+-][0-9A-Za-z]+)*[@][0-9A-Za-z]+.[a-zA-Z]{2,3}(.[a-zA-Z]{2})?$";
+        public const string Regex_PhoneNumber = "[+][1-9]{1,2}[ ][0-9]{10}$";
+
+        public const string Regex_Password_One = "^[A-Za-z0-9]{8,15}$";
+        public const string Regex_Password_Two = "^[A-Z]{1}+[a-z0-9]{8,}$";
+        public const string Regex_Password_Three = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+        public const string Regex_Password_Four = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
+        public bool FirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, Regex_FirstName);
+        }
+        public bool LastName(string lastName)
+        {
+            return Regex.IsMatch(lastName, Regex_LastName);
+        }
+        public bool Email(string email)
+        {
+            return Regex.IsMatch((string)email, Regex_Email);
+        }
+        public bool PhoneNumber(string phoneNumber)
+        {
+            return Regex.IsMatch(phoneNumber, Regex_PhoneNumber);
+        }
+        public bool Password_one(string password_one) 
+        {
+            return Regex.IsMatch(password_one, Regex_Password_One);
+        }
+        public bool Password_Two(string password_two)
+        {
+            return Regex.IsMatch(password_two, Regex_Password_Two);
+        }
+        public bool Password_Three(string password3)
+        {
+            return Regex.IsMatch(password3, Regex_Password_Three);
+        }
+        public bool Password_Four(string password4)
+        {
+            return Regex.IsMatch(password4, Regex_Password_Four);
+        }
+        public void Test()
+        {
+            Registration validate = new Registration();
+            Console.WriteLine("A. Valid Emails");
+            string[] validEmails = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+            for (int i = 0; i < validEmails.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + validEmails[i] + " : " + validate.Email(validEmails[i]));
+            }
+            Console.WriteLine("\nB. Invalid Emails");
+            string[] invalidEmails = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+            for (int i = 0; i < invalidEmails.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + invalidEmails[i] + " : " + validate.Email(invalidEmails[i]));
+            }
+        }
+
+    }
+}
